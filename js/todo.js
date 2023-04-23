c065623621ebc112ecb0ace61f22d35b13634223
Jhung Minwoo <minwoorich96@gmail.com>
2023-04-24 00:06:21 +0900

    주석 추가해줌
    큰 수정은 별로 없음

--- js/todo.js
+++ js/todo.js
@@ -1,11 +1,13 @@
 const toDoForm = document.getElementById("todo-form");
 const toDoInput = document.querySelector("#todo-form input");
 const toDoList = document.getElementById("todo-list");
-const toDos = [];
+const toDos = []; //toDo 리스트 들을 담는 배열
 
 //로컬저장소에 todo리스트 저장
 function saveToDos(toDos){
-    localStorage.setItem("todos", toDos);
+    //toDos는 배열인데 로컬저장소는 문자열 밖에 저장을 못함.
+    // 그래서 JSON.stringify() 를 통해 문자열로 변경시킴.
+    localStorage.setItem("todos", JSON.stringify(toDos));
 }
 
 //x버튼 누르면 리스트 제거
@@ -32,9 +34,9 @@ function handleToDoSubmit(event){;
     event.preventDefault(); // submit의 경우 기본적으로 새로고침 기능이있는데 이를 막기위해 preveentDefault() 호출함.;
     const newTodo = toDoInput.value; // Todo value 저장
     toDoInput.value = "";
-    toDos.push(newTodo);
-    saveToDos(toDos);
-    paintToDo(newTodo);
+    toDos.push(newTodo); // toDos 배열에 생성되는 새로운 리스트들 저장.
+    saveToDos(toDos); //로컬저장소에 toDos배열 저장
+    paintToDo(newTodo); //새로 생성된 newToDo 출력
 }
 
 toDoForm.addEventListener("submit",handleToDoSubmit);
